53a2f0f193808ba8a4029ca04941c7825b4c3826
Jelle Overmars <overmars.jelle@gmail.com>
2024-11-12 21:28:31 +0100

    added UID for easy grouping

--- FusionBodyBasedBOM/FusionBodyBasedBOM.py
+++ FusionBodyBasedBOM/FusionBodyBasedBOM.py
@@ -3,22 +3,30 @@
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 
-delimiter = "\t"
+def normalizeName(bodyName, manufactured):
+    if manufactured:
+        bodyName = bodyName.removeprefix("✓").removeprefix("+").removeprefix(" ")
+
+    return bodyName
 
 
 def exportHtml(bom):
     html = (
         "<table>"
-        + "<tr><th>Component</th><th>Body</th><th>Y</th><th>X</th><th>Z</th><th>Manufactured</th></tr>"
+        + "<tr><th>Component</th><th>Body</th><th>Y</th><th>X</th><th>Z</th><th>Manufactured</th><th>UID</th></tr>"
     )
     for item in bom:
-        html += "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td><td>{5}</td></tr>".format(
+        uid = (item["bodyZ"] + "_" + item["bodyY"] + "_" + item["bodyX"]).replace(
+            ".00", ""
+        )
+        html += "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td><td>{5}</td><td>{6}</td></tr>".format(
             item["componentName"],
             item["bodyName"],
             item["bodyY"],
             item["bodyX"],
             item["bodyZ"],
             item["manufactured"],
+            uid,
         )
     html += "</table>"
     return html
@@ -70,18 +78,20 @@ def run(context):
                 formx = formy
                 formy = tempx
 
-            manufactured = body.name.find("✓")
+            manufactured = body.name.find("✓") > -1 or body.name.find("+") > -1
+            ignored = body.name.find("_") > -1 or body.name.find("-") > -1
 
-            bom.append(
-                {
-                    "componentName": component.name,
-                    "bodyName": body.name,
-                    "bodyX": formx,
-                    "bodyY": formy,
-                    "bodyZ": formz,
-                    "manufactured": manufactured > -1,
-                }
-            )
+            if not ignored:
+                bom.append(
+                    {
+                        "componentName": component.name,
+                        "bodyName": normalizeName(body.name, manufactured),
+                        "bodyX": formx,
+                        "bodyY": formy,
+                        "bodyZ": formz,
+                        "manufactured": "1" if manufactured else "0",
+                    }
+                )
 
     msg = exportHtml(bom)
     ui.messageBox(msg, "Bill Of Body Materials")
